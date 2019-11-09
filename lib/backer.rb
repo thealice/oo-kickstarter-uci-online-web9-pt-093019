c4054c686acee3607a354e325d02468fb98b17ec
Alice Freda <alicefreda@gmail.com>
2019-11-09 01:10:42 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,14 @@
+require 'pry'
+class Backer
+  attr_reader :name, :backed_projects
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  def back_project(project_instance)
+    # Project.all.include?(project_instance)
+      @backed_projects << project_instance
+      project_instance.add_backer(self)
+      # Project.new(project_instance)
+  end
+end
---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+class Project
+  attr_reader :title, :backers
+  @@all = []
+  def initialize(title)
+    @title = title
+    @backers = []
+    save
+  end
+  def save
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def add_backer(backer)
+    backers << backer
+    backer.backed_projects << self
+  end
+end
