580528344e52819d38fabec5ce0e73a6ff39d377
Alice Freda <alicefreda@gmail.com>
2019-11-09 01:04:24 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,13 +6,10 @@ class Backer
     @backed_projects = []
   end
   def back_project(project_instance)
-    if Project.all.include?(project_instance)
+    Project.all.include?(project_instance)
       @backed_projects << project_instance
       project_instance.add_backer(self)
 
-    else
       Project.new(project_instance)
-            binding.pry
-    end
   end
 end
